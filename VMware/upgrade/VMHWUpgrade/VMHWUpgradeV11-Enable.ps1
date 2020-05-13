d7c796e3a9aca3947649fedd756263244c5793c1
Trim Hajdari <32664465+vtrimi@users.noreply.github.com>
2020-05-13 13:40:38 +0200

    Add files via upload

---
+++ VMware/upgrade/VMHWUpgrade/VMHWUpgradeV11-Enable.ps1
@@ -0,0 +1,16 @@
+Import-Module VMware.VimAutomation.Core
+
+$VIServer = Read-Host "Please enter your vCenter Server"
+Connect-VIServer $VIServer
+$vms = Import-Csv VMHWUpgrade.csv
+
+foreach ($vm in $vms) 
+{
+	Write-Host $vm.name " - Enabling VM HW Update at Soft Power Off (Version 11)" 
+	$vmConfig = New-Object VMware.Vim.VirtualMachineConfigSpec
+	$vmConfig.ScheduledHardwareUpgradeInfo = New-Object -TypeName VMware.Vim.ScheduledHardwareUpgradeInfo
+	$vmConfig.ScheduledHardwareUpgradeInfo.UpgradePolicy = "onSoftPowerOff"
+	$vmConfig.ScheduledHardwareUpgradeInfo.VersionKey = "vmx-11"
+	Get-VM $vm.name | Where-Object {$_.Extensiondata.ReconfigVM_Task($vmconfig)}
+}
+
